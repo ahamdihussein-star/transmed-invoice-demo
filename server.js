9e734d6417907dde93169940090229bb424d5c1e
ahamdihussein-star <ahamdi.hussein@gmail.com>
2025-12-02 10:07:31 +0200

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+// Serve upload page
+app.get('/upload/:sessionId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // In-memory storage for demo - single session
 const sessions = new Map();
